986e89741cdf076409357c8b58e83ec5bedf23b3
Stefrisk <stefrisk3@gmail.com>
2021-11-15 00:21:07 +0800

    重构

--- src/main/java/webroot/StefanHttpServer.java
+++ src/main/java/webroot/StefanHttpServer.java
@@ -1,10 +1,10 @@
 package webroot;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -29,32 +29,25 @@ public class StefanHttpServer {
      */
     private Integer port;
 
-    /**
-     * 最大线程数
-     */
-    private Integer maxConnections;
 
     /**
      * 线程池
      */
-    private ThreadPoolExecutor threadPoolExecutor;
+    private final ThreadPoolExecutor threadPoolExecutor;
 
     /**
      * 服务端socket
      */
     private ServerSocket serverSocket;
 
-    /**
-     * 存储文件的路径
-     */
-    private String filePath;
 
     /**
      * 构造方法
      */
     public StefanHttpServer() {
-        //初始化线程池
+        //初始化线程池,设置淘汰策略为弃老策略
         threadPoolExecutor = new ThreadPoolExecutor(10, 20, 3000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(128));
+        threadPoolExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardOldestPolicy());
         initServerSocket();
     }
 
@@ -67,10 +60,9 @@ public class StefanHttpServer {
         while (true) {
             try {
                 Socket clientSocket = serverSocket.accept();
-                HttpWorker worker=new HttpWorker(clientSocket);
+                HttpWorker worker = new HttpWorker(clientSocket);
                 //工作线程开始执行
-                Thread thread=new Thread(worker);
-                thread.start();
+                threadPoolExecutor.execute(worker);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -84,8 +76,6 @@ public class StefanHttpServer {
     private void initServerSocket() {
         port = 8888;
         hostname = "127.0.0.1";
-        maxConnections = 30;
-        filePath = "C:\\Users\\Stefrisk\\Desktop\\Git Repositories\\stefan-web-server\\src\\main\\resources\\webroot\\";
         try {
             serverSocket = new ServerSocket(port);
         } catch (IOException e) {
